6ef1cbd242dcf90d195e06663c4d1ccc16d3126f
Jephte Drake Colin <drakecolinj@gmail.com>
2019-03-04 12:02:02 -0500

    Implemet edit and delete endpoint

--- index.js
+++ index.js
@@ -17,21 +17,14 @@ const noteList = [
 //Get a specific note
 app.get('/api/notes/:id', (req,res) => {
     const note = noteList.find( n => n.id === parseInt(req.params.id))
-    if(!note) res.status(404).send("Note not found") 
+    if(!note) return res.status(404).send("Note not found") 
     res.send(note.noteBody)
 })
 
 //Add a note
 app.post('/api/notes', (req, res) => {
-    const schema = {
-        noteBody: Joi.string().min(2).required()
-    }
-    const result = Joi.validate(req.body, schema)
-
-    if(result.error) {
-        res.status(404).send(result.error.details[0].message)
-        return
-    }
+    const { error } = validateNote(req.body)
+    if(error) return res.status(404).send(error.details[0].message)
     const note = {
         id: noteList.length + 1,
         noteBody: req.body.noteBody
@@ -40,9 +33,35 @@ app.post('/api/notes', (req, res) => {
     res.send(note)
 })
 
-app.delete('/api/notes/:id', (req, res) => {
+//Edit a note
+app.put('/api/notes/:id', (req, res) => {
+    //check if note exist
+    const note = noteList.find( n => n.id === parseInt(req.params.id))
+    if(!note) return res.status(404).send("Note not found") 
 
+    const { error } = validateNote(req.body)
+    if(error) return res.status(404).send(error.details[0].message)
+    note.noteBody = req.body.noteBody
+    res.send(note)
 })
+
+//Delete note
+app.delete('/api/notes/:id', (req, res) => {
+    const note = noteList.find( n => n.id === parseInt(req.params.id))
+    if(!note) return res.status(404).send("Note not found")
+
+    const index = noteList.indexOf(note)
+    noteList.splice(index, 1)
+    res.send(note)
+})
+
+//Check if note has more than 2 character
+function validateNote(note) {
+    const schema = {
+        noteBody: Joi.string().min(2).required()
+    }
+    return result = Joi.validate(note, schema)
+}
 
 app.listen(port, () => {
     console.log(`server is up and running on ${port}`)
